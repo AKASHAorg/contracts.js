37e2f4dd0e707c6fb6fad8c2733b42fcae30d83a
Marius Darila <marius.darila@gmail.com>
2017-09-26 18:58:35 +0300

    chore(): update contracts

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ module.exports = async function init(web3Provider) {
       const instance = contract(contracts[contractName]);
       instance.setProvider(web3Provider);
       if(contractName === 'AETH'){
-        return instance.at('0xc0bfcdee0fa3476be4a1d1be36e78b9f7b9f107c').then(function (data) {
+        return instance.at('0xca6bf04351b17c449166d0f5060aca7f2adc4744').then(function (data) {
           Object.defineProperty(exported, contractName, exportValue(data));
         });
       }
